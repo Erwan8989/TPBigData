0108e5926ed337f7f99077a6b12301c8ac58ec4f
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-11-30 10:43:45 +0100

    add consumer.py

---
+++ Python/consumer.py
@@ -0,0 +1,10 @@
+from kafka import *
+import json
+import time
+
+consumer = KafkaConsumer('transactions', bootstrap_servers=['127.0.0.1:9092'])
+
+for idx, message in enumerate(consumer):
+    data = json.loads(message.value.decode('utf-8'))
+    print(f"{idx}: Un {data['type_transaction']} de {data['montant']} {data['devise']} a eu lieu à {data['lieu']}")
+    time.sleep(1)
