fea2c874ad9762c985261d42dd1fc7740334d8ff
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-11-30 12:41:00 +0100

    custom minio client file

---
+++ Python/minio_client.py
@@ -0,0 +1,17 @@
+from minio import Minio, S3Error
+
+bucket_name = "transactions"
+
+client = Minio(
+    "127.0.0.1:9000",
+    secure=False,
+    access_key="minio",
+    secret_key="minio123",
+)
+
+# Make 'transactions' bucket if not exist.
+found = client.bucket_exists(bucket_name)
+if not found:
+    client.make_bucket(bucket_name)
+else:
+    print("Bucket 'transactions' already exists")
